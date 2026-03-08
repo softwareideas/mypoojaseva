93be98d48e4e93ce84702ed12093e02bba464fb9
pdanussh <danussh@squareshift.co>
2026-03-08 12:18:21 +0530

    deploy

--- src/App.tsx
+++ src/App.tsx
@@ -22,7 +22,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      <BrowserRouter basename="/mypoojaseva">
         <ScrollToTop />
         <WhatsAppButton />
         <Routes>
